2525a7ebefa1a3f172656738dd60377577283330
kavi <ostudy590@gmail.com>
2024-06-07 19:11:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; // lowercase 'react' here
+import Counter from './Counter.jsx';
+
+function App() {
+  return (<Counter/>
+  );
+}
+
+export default App;
+
+/*cd my-app
+npm start*/
